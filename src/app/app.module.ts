0f2a2ca8646f7903690f4d2f2e3431b3473f4b67
vj2970 <99133919+vj2970@users.noreply.github.com>
2024-02-13 17:06:42 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success.alert.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrl: './warning-alert.component.css'
+})
+export class WarningAlertComponent {
+
+  message:string = "";
+  
+  printWarning(){
+    this.message = "This is a warning message##";
+}
+}
